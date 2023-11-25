96aa6eb94f2aa9f7784d72fb89b9c3d7e8fb3352
user <user@192.168.10.176>
2023-11-25 15:00:21 +0900

    entity作成

--- todoapp/src/main/java/com/todo/entity/Todo.java
+++ todoapp/src/main/java/com/todo/entity/Todo.java
@@ -1,5 +1,13 @@
 package com.todo.entity;
 
-public class Todo {
+import lombok.Data;
 
+
+@Data
+public class Todo {
+	
+	private long id;
+	private String title;
+	private int done_flg;
+	private String time_limit;
 }
